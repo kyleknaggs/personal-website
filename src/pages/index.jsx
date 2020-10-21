8486fd343408a705dd9e334ae1219efa7a107e7d
Kyle Knaggs <kyleknaggs@gmail.com>
2020-10-21 17:00:48 -0400

    Add first styled component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'gatsby';
+
+const StyledH1 = styled.h1`
+  color: red;
+`;
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <StyledH1>Home</StyledH1>
       <Link to="/online-ordering-platform">Online Ordering Platform</Link>
     </div>
   );
