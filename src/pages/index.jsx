17c8702d48c43e3418218df68334092f28f42fbb
Kyle Knaggs <kyleknaggs@gmail.com>
2020-10-26 22:27:10 -0400

    Add padding to main element on home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,20 +3,24 @@ import styled from 'styled-components';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
-const StyledH1 = styled.h1`
-  color: green;
-`;
-
 const StyledDiv = styled.div`
   background: lightgrey;
   height: calc(100vh - var(--menu-height));
+  padding-right: 9vw;
+  padding-bottom: 9vw;
+  padding-left: 9vw;
+  @media screen and (max-width: 1024px) {
+    padding-right: 13.5vw;
+    padding-bottom: 13.5vw;
+    padding-left: 13.5vw;
+  }
 `;
 
 export default function Home() {
   return (
     <Layout>
       <StyledDiv>
-        <StyledH1>Home</StyledH1>
+        <h1>Home</h1>
         <Link to="/online-ordering-platform">Online Ordering Platform</Link>
       </StyledDiv>
     </Layout>
