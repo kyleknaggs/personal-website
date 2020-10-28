ae48b406120e84dcb56788f5a39569f01de26567
Kyle Knaggs <kyleknaggs@gmail.com>
2020-10-28 15:26:45 -0400

    Add functioning links to page navigation

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
+import { HREF } from '../utility/constants';
 import './layout.css';
 
 const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   height: var(--menu-height);
-  background-color: grey;
+  padding-left: 4vw;
+  padding-right: 4vw;
+  background-color: lightgrey;
 `;
 
 export default function Layout({ children }) {
+  const { INDEX, LINKEDIN, GITHUB } = HREF;
+
   return (
     <div>
-      <StyledHeader />
+      <StyledHeader>
+        <div>
+          <Link href={INDEX}>Kyle Knaggs</Link>
+        </div>
+        <div>
+          <Link href={LINKEDIN}>LinkedIn</Link>
+          <Link href={GITHUB}>GitHub</Link>
+        </div>
+      </StyledHeader>
       {children}
     </div>
   );
--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,9 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 // The intention is for this file to contain different types of strings
 const HREF = {
+  INDEX: '/',
   ONLINE_ORDERING_PLATFORM: '/online-ordering-platform',
-  FIREFOX_DEBUGGER: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&classification=Components&component=Debugger&email1=kyleknaggs%40gmail.com&emailassigned_to1=1&emailtype1=exact&list_id=15389168&product=DevTools&query_format=advanced&resolution=---&resolution=FIXED&resolution=INVALID&resolution=WONTFIX&resolution=INACTIVE&resolution=DUPLICATE&resolution=WORKSFORME&resolution=INCOMPLETE&resolution=SUPPORT&resolution=EXPIRED&resolution=MOVED',
   RESUME: '/resume',
+  LINKEDIN: 'https://www.linkedin.com/in/kyle-knaggs/',
+  GITHUB: 'https://github.com/kyleknaggs',
+  FIREFOX_DEBUGGER: 'https://bugzilla.mozilla.org/buglist.cgi?bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&classification=Components&component=Debugger&email1=kyleknaggs%40gmail.com&emailassigned_to1=1&emailtype1=exact&list_id=15389168&product=DevTools&query_format=advanced&resolution=---&resolution=FIXED&resolution=INVALID&resolution=WONTFIX&resolution=INACTIVE&resolution=DUPLICATE&resolution=WORKSFORME&resolution=INCOMPLETE&resolution=SUPPORT&resolution=EXPIRED&resolution=MOVED',
 };
 
 export { HREF };
