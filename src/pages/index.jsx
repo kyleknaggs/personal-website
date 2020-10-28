ddd60703c7af111f7b6546aa1ebf2124b1fc0b18
Kyle Knaggs <kyleknaggs@gmail.com>
2020-10-28 10:09:01 -0400

    Add skeleton body text and links to main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import Layout from '../components/layout';
 
 const StyledDiv = styled.div`
   background: lightgrey;
-  height: calc(100vh - var(--menu-height));
+  min-height: calc(100vh - var(--menu-height));
   padding-right: 9vw;
   padding-bottom: 9vw;
   padding-left: 9vw;
@@ -25,12 +25,21 @@ const StyledH1 = styled.h1`
 
 export default function Home() {
   const heroText = 'Kyle Knaggs is a Software Developer from Trinidad and Tobago obsessed with making great user experiences a reality.';
+  const selectedWork = ' Selected Work: Open Source Contributions to the Firefox Debugger and an Online Ordering System for the largest mattress manufacturer in the Caribbean.';
+  const keySkills = 'Key Skills: JavaScript, React, Redux, ES6, Jest, Flow, Git/Mercurial, Bootstrap, Styled-Components, HTML, CSS, Sketch and Adobe Creative Suite.';
+  const viewResume = 'View Resume';
+  // TODO: Remove once links are properly set up
+  const onlineOrderingPlatform = 'Online Ordering Platform';
 
   return (
     <Layout>
       <StyledDiv>
         <StyledH1>{heroText}</StyledH1>
-        <Link to="/online-ordering-platform">Online Ordering Platform</Link>
+        <p>{selectedWork}</p>
+        <p>{keySkills}</p>
+        <Link to="/online-ordering-platform">{viewResume}</Link>
+        {/* TODO: Remove once links are properly set up */}
+        <Link to="/online-ordering-platform">{onlineOrderingPlatform}</Link>
       </StyledDiv>
     </Layout>
   );
