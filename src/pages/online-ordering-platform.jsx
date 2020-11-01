3d1aaf70ba03dd3c569d4597f99c68d277984a0c
Kyle Knaggs <kyleknaggs@gmail.com>
2020-10-31 23:51:16 -0400

    Add iframe with AFL online video

--- src/pages/online-ordering-platform.jsx
+++ src/pages/online-ordering-platform.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 
 export default function OnlineOrderingPlatform() {
+  // TODO: Go through each attribute in the iframe and figure out whether or not it is necessary
   return (
     <Layout>
-      <h1>Online Ordering Platform</h1>
-      <Link to="/">Home</Link>
+      <iframe
+        src="https://player.vimeo.com/video/455057785?app_id=122963&amp;wmode=opaque&amp;autoplay=1"
+        width="426"
+        height="240"
+        frameBorder="0"
+        allow="autoplay; fullscreen"
+        allowFullScreen=""
+        title="aflOnline_applicationDemo"
+        id="yui_3_17_2_1_1604168177590_76"
+        style={{
+          opacity: 1,
+        }}
+      />
     </Layout>
   );
 }
