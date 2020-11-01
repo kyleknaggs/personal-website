b81d99b3e9d0ea16009df05a26b052aeecdf8094
Kyle Knaggs <kyleknaggs@gmail.com>
2020-11-01 11:05:23 -0400

    Add video placeholder to online-ordering-platform

--- src/pages/online-ordering-platform.jsx
+++ src/pages/online-ordering-platform.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../components/Layout';
+import Main from '../components/Main';
+import { IMG } from '../utility/constants';
+
+// Because the image file is larger than the max width of the parent element
+// Setting width to 100% ensures that it never extends beyond the width
+//  of its parent element
+const StyledImg = styled.img`
+  width: 100%;
+`;
+
+/*
+  TODO: Markup for iframe used to load in Vimeo video
+  <iframe
+    src="https://player.vimeo.com/video/455057785?app_id=122963&amp;wmode=opaque&amp;autoplay=1"
+    width="100%"
+    height="240"
+    frameBorder="0"
+    allow="autoplay; fullscreen"
+    allowFullScreen=""
+    title="aflOnline_applicationDemo"
+    id="yui_3_17_2_1_1604168177590_76"
+    style={{
+      opacity: 1,
+    }}
+  />
+*/
 
 export default function OnlineOrderingPlatform() {
-  // TODO: Go through each attribute in the iframe and figure out whether or not it is necessary
+  const { AFL_ONLINE_VIDEO } = IMG;
+
   return (
     <Layout>
-      <iframe
-        src="https://player.vimeo.com/video/455057785?app_id=122963&amp;wmode=opaque&amp;autoplay=1"
-        width="426"
-        height="240"
-        frameBorder="0"
-        allow="autoplay; fullscreen"
-        allowFullScreen=""
-        title="aflOnline_applicationDemo"
-        id="yui_3_17_2_1_1604168177590_76"
-        style={{
-          opacity: 1,
-        }}
-      />
+      <Main>
+        <StyledImg
+          src={AFL_ONLINE_VIDEO}
+          alt=""
+        />
+      </Main>
     </Layout>
   );
 }
