f8101c6578b6e04de7ee2bec43cbe8900e9dbe19
Kyle Knaggs <kyleknaggs@gmail.com>
2020-11-27 15:12:21 -0400

    Change animation length to 3.5s

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,11 +6,11 @@ const StyledMain = styled.main`
   max-width: 1920px;
   min-height: calc(100vh - var(--menu-height-lg));
   padding: 0vw 10.5vw;
-  animation: fadeIn ease-in 2s;
-  -webkit-animation: fadeIn ease-in 2s;
-  -moz-animation: fadeIn ease-in 2s;
-  -o-animation: fadeIn ease-in 2s;
-  -ms-animation: fadeIn ease-in 2s;
+  animation: fadeIn ease-in 3.5s;
+  -webkit-animation: fadeIn ease-in 3.5s;
+  -moz-animation: fadeIn ease-in 3.5s;
+  -o-animation: fadeIn ease-in 3.5s;
+  -ms-animation: fadeIn ease-in 3.5s;
 
   @media screen and (max-width: 1024px) {
     min-height: calc(100vh - var(--menu-height-md));
