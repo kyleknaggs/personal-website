277ab7d498fe2a060291ab58afb912ad656227ae
Kyle Knaggs <kyleknaggs@gmail.com>
2020-11-28 20:43:38 -0400

    Add placeholder for custom 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Main from '../components/Main';
+
+export default function Home() {
+  // TODO: Add content to 404 page
+  return (
+    <Layout>
+      <Main>
+        <p>My new custom 404 page</p>
+      </Main>
+    </Layout>
+  );
+}
