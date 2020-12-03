ff207fd78f96d8a924f8bbbe698b4533c4e24b4a
Kyle Knaggs <kyleknaggs@gmail.com>
2020-12-03 18:59:26 -0400

    Test Squarespace social sharing image

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -14,12 +14,14 @@ export default function SEO({ title, description }) {
       {/* Test images have been borrowed as placeholders */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
-      <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+      <meta property="og:url" content="https://kyle-knaggs-personal-website.netlify.app/" />
+      <meta property="og:image" content="http://static1.squarespace.com/static/5c7574443560c34b3b68682f/t/5cd4729908522907045f107c/1557426842694/social-sharing.png?format=1500w" />
+      <meta property="og:image:width" content="1080" />
+      <meta property="og:image:height" content="564" />
       {/* Twitter's meta tags which are similar to the Open Graph Protocol */}
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content=" https://ia.media-imdb.com/images/rock.jpg" />
+      <meta name="twitter:image" content="http://static1.squarespace.com/static/5c7574443560c34b3b68682f/t/5cd4729908522907045f107c/1557426842694/social-sharing.png?format=1500w" />
       <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
   );
