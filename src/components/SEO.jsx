bb45bd0ae5497c349d5fc90f9ccea7f12b24fa11
Kyle Knaggs <kyleknaggs@gmail.com>
2020-12-03 18:40:43 -0400

    Add test graph images to SEO component

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -7,6 +7,20 @@ export default function SEO({ title, description }) {
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
+      {/* TODO: Update graph images */}
+      {/* The Essential Meta tags for social media article: https://css-tricks.com/essential-meta-tags-social-media/ */}
+      {/* Website on the Open Graph protocol: https://ogp.me/ */}
+      {/* Test meta tags that leverage the Open Graph protocol */}
+      {/* Test images have been borrowed as placeholders */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="https://www.imdb.com/title/tt0117500/" />
+      <meta property="og:url" content="https://ia.media-imdb.com/images/rock.jpg" />
+      {/* Twitter's meta tags which are similar to the Open Graph Protocol */}
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content=" https://ia.media-imdb.com/images/rock.jpg" />
+      <meta name="twitter:card" content="summary_large_image" />
     </Helmet>
   );
 }
