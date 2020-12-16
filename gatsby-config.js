631a1338cb8f46b961303dfb824b5526c60ad5d6
Kyle Knaggs <kyleknaggs@gmail.com>
2020-12-15 21:31:55 -0400

    Add gatsby-plugin-google-gtag to gatsby.config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,15 @@ module.exports = {
     'gatsby-plugin-eslint',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        // TODO: Confirm this Measurement ID works and remove if it does not.
+        trackingIds: [
+          'G-7LVDBJZ8E6',
+        ],
+      },
+    },
   ],
   siteMetadata: {
     title: 'Kyle Knaggs | Software Developer',
