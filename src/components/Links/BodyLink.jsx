ffcebe69eab0e05cbfd7c705f22a9fa541a522b0
Kyle Knaggs <kyleknaggs@gmail.com>
2023-02-27 20:17:48 -0400

    Move underline style below link text

--- src/components/Links/BodyLink.jsx
+++ src/components/Links/BodyLink.jsx
@@ -3,15 +3,27 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
+// Style from Melanie's website:
+// box-shadow: inset 0 -2px 0 0 #d0df00;
 const StyledA = styled.a`
   color: hsl(0, 0%, 20%);
+  text-decoration: none;
+  box-shadow: inset 0 -1px 0 hsl(0, 0%, 20%);
+
+  :hover {
+    color: #000;
+    box-shadow: inset 0 -1px 0 #000;
+  }
 `;
 
 const StyledLink = styled(Link)`
   color: hsl(0, 0%, 20%);
-  
+  text-decoration: none;
+  box-shadow: inset 0 -1px 0 hsl(0, 0%, 20%);
+
   :hover {
     color: #000;
+    box-shadow: inset 0 -1px 0 #000;
   }
 `;
 
