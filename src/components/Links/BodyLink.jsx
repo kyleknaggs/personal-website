085aff0608e980f057306026c1d3841afa0403ec
Kyle Knaggs <kyleknaggs@gmail.com>
2023-03-04 09:58:50 -0400

    Simplify and smooth out link animations

--- src/components/Links/BodyLink.jsx
+++ src/components/Links/BodyLink.jsx
@@ -6,47 +6,32 @@ import { Link } from 'gatsby';
 // Add a hover animation to both types of links on desktop devices. Don't apply
 // this styling to mobile devices as hover styles are not relevant to them
 const StyledA = styled.a`
-  box-shadow: inset 0 -1px 0 hsl(0, 0%, 20%);
+box-shadow: inset 0 -1px 0 hsl(0, 0%, 20%);
   color: hsl(0, 0%, 20%);
+  font-family: Gill Sans Nova SemiBold;
+  transition: color 0.5s ease-in-out, box-shadow 0.5s ease-in-out;
   text-decoration: none;
-
-  @keyframes slideUp {
-    from {
-      box-shadow: inset 0 -1px 0 #00EC7A;
-    }
-  
-    to {
-      box-shadow: inset 0 -20px 0 #00EC7A;
-    }
-  }
 
   :hover {
     @media (min-width: 1200px) {
-      animation-duration: 0.5s;
-      animation-name: slideUp;
-      animation-timing-function: ease-out;
-      box-shadow: inset 0 -20px 0 #00EC7A;
-      color: hsl(0, 0%, 20%);
+      box-shadow: inset 0 -20px 0 hsl(0, 0%, 20%);
+      color: white;
     }
   }
 `;
 
-// Can borrow the keyframes definition from above instead of having to define
-// it again
 const StyledLink = styled(Link)`
-  box-shadow: inset 0 -1px 0 hsl(0, 0%, 20%);
   color: hsl(0, 0%, 20%);
+  font-family: Gill Sans Nova SemiBold;
+  transition: color 0.5s ease-in-out, box-shadow 0.5s ease-in-out;
   text-decoration: none;
 
   :hover {
     @media (min-width: 1200px) {
-      animation-duration: 0.5s;
-      animation-name: slideUp;
-      animation-timing-function: ease-out;
-      box-shadow: inset 0 -20px 0 #00EC7A;
-      color: hsl(0, 0%, 20%);
-    }
+      box-shadow: inset 0 -20px 0 hsl(0, 0%, 20%);
+      color: white;
   }
+}
 `;
 
 export default function BodyLink({ children, to, external }) {
